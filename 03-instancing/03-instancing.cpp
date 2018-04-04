5dfd605b27f8d1d2d5f69c7a871e65e82fb714cd
szq <suzhaoqi280579550@163.com>
2018-04-04 19:57:48 +0800

    03-instancing2

--- 03-instancing/03-instancing.cpp
+++ 03-instancing/03-instancing.cpp
@@ -16,57 +16,19 @@
 #include "vbm.h"
 
 float aspect;
-GLuint update_prog;
-GLuint vao[2];
-GLuint vbo[2];
-GLuint xfb;
 
-GLuint weight_vbo;
-GLuint color_vbo;
+GLuint color_buffer;
+GLuint model_matrix_buffer;
 GLuint render_prog;
-GLint render_model_matrix_loc;
-GLint render_projection_matrix_loc;
-
-GLuint geometry_tex;
-
-GLuint geometry_xfb;
-GLuint particle_xfb;
-
 GLint model_matrix_loc;
+GLint view_matrix_loc;
 GLint projection_matrix_loc;
-GLint triangle_count_loc;
-GLint time_step_loc;
 
 VBObject object;
 
 ULONGLONG m_appStartTime;
 
-#define INSTANCE_COUNT 200
-
-static unsigned int seed = 0x13371337;
-
-static inline float random_float()
-{
-    float res;
-    unsigned int tmp;
-
-    seed *= 16807;
-
-    tmp = seed ^ (seed >> 4) ^ (seed << 15);
-
-    *((unsigned int *) &res) = (tmp >> 9) | 0x3F800000;
-
-    return (res - 1.0f);
-}
-
-static glm::vec3 random_vector(float minmag = 0.0f, float maxmag = 1.0f)
-{
-    glm::vec3 randomvec(random_float() * 2.0f - 1.0f, random_float() * 2.0f - 1.0f, random_float() * 2.0f - 1.0f);
-    randomvec = normalize(randomvec);
-    randomvec *= (random_float() * (maxmag - minmag) + minmag);
-
-    return randomvec;
-}
+#define INSTANCE_COUNT 100
 
 void Initialize()
 {
@@ -83,15 +45,38 @@ void Initialize()
 
     glUseProgram(render_prog);
 
-    // "model_matrix" is actually an array of 4 matrices
-    render_model_matrix_loc = glGetUniformLocation(render_prog, "model_matrix");
-    render_projection_matrix_loc = glGetUniformLocation(render_prog, "projection_matrix");
+    // Get the location of the projetion_matrix uniform
+    view_matrix_loc = glGetUniformLocation(render_prog, "view_matrix");
+    projection_matrix_loc = glGetUniformLocation(render_prog, "projection_matrix");
 
     // Load the object
     object.LoadFromVBM("armadillo_low.vbm", 0, 1, 2);
 
     // Bind its vertex array object so that we can append the instanced attributes
     object.BindVertexArray();
+
+    // Get the locations of the vertex attributes in 'prog', which is the
+    // (linked) program object that we're going to be rendering with. Note
+    // that this isn't really necessary because we specified locations for
+    // all the attributes in our vertex shader. This code could be made
+    // more concise by assuming the vertex attributes are where we asked
+    // the compiler to put them.
+    int position_loc = glGetAttribLocation(render_prog, "position");
+    int normal_loc = glGetAttribLocation(render_prog, "normal");
+    int color_loc = glGetAttribLocation(render_prog, "color");
+    int matrix_loc = glGetAttribLocation(render_prog, "model_matrix");
+
+    // Configure the regular vertex attribute arrays - position and color.
+    /*
+    // This is commented out here because the VBM object takes care
+    // of it for us.
+    glBindBuffer(GL_ARRAY_BUFFER, position_buffer);
+    glVertexAttribPointer(position_loc, 4, GL_FLOAT, GL_FALSE, 0, NULL);
+    glEnableVertexAttribArray(position_loc);
+    glBindBuffer(GL_ARRAY_BUFFER, normal_buffer);
+    glVertexAttribPointer(normal_loc, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    glEnableVertexAttribArray(normal_loc);
+    */
 
     // Generate the colors of the objects
     glm::vec4 colors[INSTANCE_COUNT];
@@ -102,33 +87,47 @@ void Initialize()
         float b = float(n) / 5.0f;
         float c = float(n) / 6.0f;
 
-        colors[n][0] = 0.5f * (sinf(a + 1.0f) + 1.0f);
-        colors[n][1] = 0.5f * (sinf(b + 2.0f) + 1.0f);
-        colors[n][2] = 0.5f * (sinf(c + 3.0f) + 1.0f);
+        colors[n][0] = 0.5f + 0.25f * (sinf(a + 1.0f) + 1.0f);
+        colors[n][1] = 0.5f + 0.25f * (sinf(b + 2.0f) + 1.0f);
+        colors[n][2] = 0.5f + 0.25f * (sinf(c + 3.0f) + 1.0f);
         colors[n][3] = 1.0f;
     }
 
-    // Create and allocate the VBO to hold the weights
-    // Notice that we use the 'colors' array as the initial data, but only because
-    // we know it's the same size.
-    glGenBuffers(1, &weight_vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, weight_vbo);
+    glGenBuffers(1, &color_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, color_buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_DYNAMIC_DRAW);
 
-    // Here is the instanced vertex attribute - set the divisor
-    glVertexAttribDivisor(3, 1);
-    // It's otherwise the same as any other vertex attribute - set the pointer and enable it
-    glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, 0, NULL);
-    glEnableVertexAttribArray(3);
+    // Now we set up the color array. We want each instance of our geometry
+    // to assume a different color, so we'll just pack colors into a buffer
+    // object and make an instanced vertex attribute out of it.
+    glBindBuffer(GL_ARRAY_BUFFER, color_buffer);
+    glVertexAttribPointer(color_loc, 4, GL_FLOAT, GL_FALSE, 0, NULL);
+    glEnableVertexAttribArray(color_loc);
+    // This is the important bit... set the divisor for the color array to
+    // 1 to get OpenGL to give us a new value of 'color' per-instance
+    // rather than per-vertex.
+    glVertexAttribDivisor(color_loc, 1);
 
-    // Same with the instance color array
-    glGenBuffers(1, &color_vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, color_vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
-
-    glVertexAttribDivisor(4, 1);
-    glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, 0, NULL);
-    glEnableVertexAttribArray(4);
+    // Likewise, we can do the same with the model matrix. Note that a
+    // matrix input to the vertex shader consumes N consecutive input
+    // locations, where N is the number of columns in the matrix. So...
+    // we have four vertex attributes to set up.
+    glGenBuffers(1, &model_matrix_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, model_matrix_buffer);
+    glBufferData(GL_ARRAY_BUFFER, INSTANCE_COUNT * sizeof(glm::mat4), NULL, GL_DYNAMIC_DRAW);
+    // Loop over each column of the matrix...
+    for (int i = 0; i < 4; i++)
+    {
+        // Set up the vertex attribute
+        glVertexAttribPointer(matrix_loc + i,              // Location
+                              4, GL_FLOAT, GL_FALSE,       // vec4
+                              sizeof(glm::mat4),                // Stride
+                              (void *)(sizeof(glm::vec4) * i)); // Start offset
+                                                           // Enable it
+        glEnableVertexAttribArray(matrix_loc + i);
+        // Make it instanced
+        glVertexAttribDivisor(matrix_loc + i, 1);
+    }
 
     // Done (unbind the object's VAO)
     glBindVertexArray(0);
@@ -143,31 +142,12 @@ void Display()
 {
     ULONGLONG currentTime = ::GetTickCount64();
     unsigned int app_time = (unsigned int)(currentTime - m_appStartTime);
-    float t = float(app_time & 0x3FFFF) / float(0x3FFFF);
+    float t = float(app_time & 0x3FFF) / float(0x3FFF);
     static float q = 0.0f;
     static const glm::vec3 X(1.0f, 0.0f, 0.0f);
     static const glm::vec3 Y(0.0f, 1.0f, 0.0f);
     static const glm::vec3 Z(0.0f, 0.0f, 1.0f);
     int n;
-
-    // Set weights for each instance
-    glm::vec4 weights[INSTANCE_COUNT];
-
-    for (n = 0; n < INSTANCE_COUNT; n++)
-    {
-        float a = float(n) / 4.0f;
-        float b = float(n) / 5.0f;
-        float c = float(n) / 6.0f;
-
-        weights[n][0] = 0.5f * (sinf(t * 6.28318531f * 8.0f + a) + 1.0f);
-        weights[n][1] = 0.5f * (sinf(t * 6.28318531f * 26.0f + b) + 1.0f);
-        weights[n][2] = 0.5f * (sinf(t * 6.28318531f * 21.0f + c) + 1.0f);
-        weights[n][3] = 0.5f * (sinf(t * 6.28318531f * 13.0f + a + b) + 1.0f);
-    }
-
-    // Bind the weight VBO and change its data
-    glBindBuffer(GL_ARRAY_BUFFER, weight_vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(weights), weights, GL_DYNAMIC_DRAW);
 
     // Clear
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -177,28 +157,35 @@ void Display()
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
 
+    // Bind the weight VBO and change its data
+    glBindBuffer(GL_ARRAY_BUFFER, model_matrix_buffer);
+
+    // Set model matrices for each instance
+    glm::mat4 * matrices = (glm::mat4 *)glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+
+    for (n = 0; n < INSTANCE_COUNT; n++)
+    {
+        float a = 50.0f * float(n) / 4.0f;
+        float b = 50.0f * float(n) / 5.0f;
+        float c = 50.0f * float(n) / 6.0f;
+
+        matrices[n] = glm::rotate(glm::mat4(), glm::radians(a + t * 360.0f), glm::vec3(1.0f, 0.0f, 0.0f)) *
+            glm::rotate(glm::mat4(), glm::radians(b + t * 360.0f), glm::vec3(0.0f, 1.0f, 0.0f)) *
+            glm::rotate(glm::mat4(), glm::radians(c + t * 360.0f), glm::vec3(0.0f, 0.0f, 1.0f)) *
+            glm::translate(glm::mat4(), glm::vec3(10.0f + a, 40.0f + b, 50.0f + c));
+    }
+
+    glUnmapBuffer(GL_ARRAY_BUFFER);
+
     // Activate instancing program
     glUseProgram(render_prog);
 
-    // Set four model matrices
-    glm::mat4 model_matrix[4];
+    // Set up the view and projection matrices
+    glm::mat4 view_matrix(glm::translate(glm::mat4(), glm::vec3(0.0f, 0.0f, -1500.0f)) * glm::rotate(glm::mat4(), glm::radians(t * 360.0f * 2.0f), glm::vec3(0.0f, 1.0f, 0.0f)));
+    glm::mat4 projection_matrix(glm::frustum(-1.0f, 1.0f, -aspect, aspect, 1.0f, 5000.0f));
 
-    for (n = 0; n < 4; n++)
-    {
-        model_matrix[n] = (glm::scale(glm::mat4(), glm::vec3(5.0f, 5.0f, 5.0f)) *
-                           glm::rotate(glm::mat4(), glm::radians(t * 360.0f * 40.0f + float(n + 1) * 29.0f), glm::vec3(0.0f, 1.0f, 0.0f)) *
-                           glm::rotate(glm::mat4(), glm::radians(t * 360.0f * 20.0f + float(n + 1) * 35.0f), glm::vec3(0.0f, 0.0f, 1.0f)) *
-                           glm::rotate(glm::mat4(), glm::radians(t * 360.0f * 30.0f + float(n + 1) * 67.0f), glm::vec3(0.0f, 1.0f, 0.0f)) *
-                           glm::translate(glm::mat4(), glm::vec3((float)n * 10.0f - 15.0f, 0.0f, 0.0f)) *
-                           glm::scale(glm::mat4(), glm::vec3(0.01f, 0.01f, 0.01f)));
-    }
-
-    glUniformMatrix4fv(render_model_matrix_loc, 4, GL_FALSE, &model_matrix[0][0][0]);
-
-    // Set up the projection matrix
-    glm::mat4 projection_matrix(glm::frustum(-1.0f, 1.0f, -aspect, aspect, 1.0f, 5000.0f) * glm::translate(glm::mat4(), glm::vec3(0.0f, 0.0f, -100.0f)));
-
-    glUniformMatrix4fv(render_projection_matrix_loc, 1, GL_FALSE, &projection_matrix[0][0]);
+    glUniformMatrix4fv(view_matrix_loc, 1, GL_FALSE, &view_matrix[0][0]);
+    glUniformMatrix4fv(projection_matrix_loc, 1, GL_FALSE, &projection_matrix[0][0]);
 
     // Render INSTANCE_COUNT objects
     object.Render(0, INSTANCE_COUNT);
@@ -207,9 +194,9 @@ void Display()
 void Finalize(void)
 {
     glUseProgram(0);
-    glDeleteProgram(update_prog);
-    glDeleteVertexArrays(2, vao);
-    glDeleteBuffers(2, vbo);
+    glDeleteProgram(render_prog);
+    glDeleteBuffers(1, &color_buffer);
+    glDeleteBuffers(1, &model_matrix_buffer);
 }
 
 int main(int argc, char** argv)
--- 03-instancing/render.vs.glsl
+++ 03-instancing/render.vs.glsl
@@ -1,28 +1,41 @@
-#version 410
+#version 330
 
-uniform mat4 model_matrix[4];
-uniform mat4 projection_matrix;
-
+// 'position' and 'normal' are regular vertex attributes
 layout (location = 0) in vec4 position;
 layout (location = 1) in vec3 normal;
 
-layout (location = 3) in vec4 instance_weights;
-layout (location = 4) in vec4 instance_color;
+// Color is a per-instance attribute
+layout (location = 2) in vec4 color;
 
-out vec3 vs_fs_normal;
-out vec4 vs_fs_color;
+// model_matrix will be used as a per-instance transformation
+// matrix. Note that a mat4 consumes 4 consecutive locations, so
+// this will actually sit in locations, 3, 4, 5, and 6.
+layout (location = 3) in mat4 model_matrix;
 
+// The view matrix and the projection matrix are constant across a draw
+uniform mat4 view_matrix;
+uniform mat4 projection_matrix;
+
+// The output of the vertex shader (matched to the fragment shader)
+out VERTEX
+{
+    vec3    normal;
+    vec4    color;
+} vertex;
+
+// Ok, go!
 void main(void)
 {
-    int n;
-    mat4 m = mat4(0.0);
-    vec4 pos = position;
-    vec4 weights = normalize(instance_weights);
-    for (n = 0; n < 4; n++)
-    {
-        m += (model_matrix[n] * weights[n]);
-    }
-    vs_fs_normal = normalize((m * vec4(normal, 0.0)).xyz);
-    vs_fs_color = instance_color;
-    gl_Position = projection_matrix * (m * pos);
+    // Construct a model-view matrix from the uniform view matrix
+    // and the per-instance model matrix.
+    mat4 model_view_matrix = view_matrix * model_matrix;
+
+    // Transform position by the model-view matrix, then by the
+    // projection matrix.
+    gl_Position = projection_matrix * (model_view_matrix * position);
+    // Transform the normal by the upper-left-3x3-submatrix of the
+    // model-view matrix
+    vertex.normal = mat3(model_view_matrix) * normal;
+    // Pass the per-instance color through to the fragment shader.
+    vertex.color = color;
 }
